0e8c9c1e27885c4978268d830bd2983922ff25b1
Jiao Lin <linjiao@ornl.gov>
2015-10-29 11:26:05 -0400

    added setup.py to install the pkg at danse.ins.dsm

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='danse.ins.dsm',
+      version='0.1',
+      description='Data stream model',
+      author='Jiao Lin',
+      author_email='jiao.lin@gmail.com',
+      url='https://github.com/danse-inelastic/dsm',
+      packages=['danse.ins.dsm'],
+      package_dir = {'danse.ins.dsm': '.'},
+     )
